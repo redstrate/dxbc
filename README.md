584a7c6533f66948e587a149c678f0cdbddbbf9f
Joshua Goins <josh@redstrate.com>
2023-09-22 15:54:45 -0400

    Add initial files

---
+++ README.md
@@ -0,0 +1,7 @@
+# dxbc
+
+This is the `dxbc` decompiler pulled directly from the [DXVK source tree](https://github.com/doitsujin/dxvk). This is for my personal use, do not expect me to keep updating this.
+
+See an example of how to use the library in `example/`.
+
+All credit goes to the DXVK developers, of course.
---
+++ example/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+#include <dxbc/dxbc_module.h>
+#include <util/log/log.h>
+
+#include <spirv_cross/spirv_glsl.hpp>
+
+dxvk::Logger dxvk::Logger::s_instance("dxbc.log");
+
+int main(int argc, char *argv[]) {
+  // pass in the DXVK binary you want to decompile
+  const std::string inputFile = argv[1];
+  std::ifstream infile(inputFile, std::ios_base::binary);
+
+  std::vector<char> buffer((std::istreambuf_iterator<char>(infile)),
+                           std::istreambuf_iterator<char>());
+
+  dxvk::DxbcReader reader(buffer.data(), buffer.size());
+
+  dxvk::DxbcModule module(reader);
+
+  dxvk::DxbcModuleInfo info;
+  auto result = module.compile(info, "test");
+
+  spirv_cross::CompilerGLSL glsl(result.code.data(), result.code.dwords());
+
+  spirv_cross::CompilerGLSL::Options options;
+  options.version = 310;
+  options.es = true;
+  glsl.set_common_options(options);
+
+  std::cout << glsl.compile() << std::endl;
+
+  return 0;
+}
